d17f3927a04730927b35fb3e8fa4602fa4ca0bf3
Peter <peter@Peters-MBP.peter.lan>
2023-06-09 15:52:14 -0600

    updated main.py and got rid of the tutorial HTML pages.

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
-@app.route("/store")
-def store():
-    return render_template("store.html")
-
-
-@app.route("/contact-us")
-def contact_us():
-    return render_template("contact_us.html")
-
-
-app.run(debug=True)
-
-# Test
+if __name__ == "__main__":
+    app.run(debug=True)
