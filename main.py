89d6e4da239edc24105d9b683392c35e5e76711b
Peter <peter@Peters-MBP.peter.lan>
2023-06-09 11:06:35 -0600

    did a bunch of stuff

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask("Website")
+
+
+@app.route("/")
+def home():
+    return render_template("tutorial.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/store")
+def store():
+    return render_template("store.html")
+
+
+@app.route("/contact-us")
+def contact_us():
+    return render_template("contact_us.html")
+
+
+app.run(debug=True)
