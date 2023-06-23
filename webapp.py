8632dfe1d27f0d651a688741a5f6f0c8891eaded
Peter <peterchay88@gmail.com>
2023-06-22 23:27:47 -0600

    deleting webapp.py

--- webapp.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Weather Forecast for the Next Days")
-place = st.text_input(label="Place:", key="place")
-slider = st.slider(label="Forecast Days", min_value=1, max_value=5, key="forecast", help="select the number of "
-                                                                                         "forecasted days")
-select_box = st.selectbox(label="Select Data to view", options=["Temperature", "Sky"], key="view_data")
-st.subheader(f"{select_box} for the next {slider} Days in {place}")
-
